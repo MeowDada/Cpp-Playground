8240d847433b5bbc9b6ae1b2c6ca16ee35943be2
MeowDada <chang.hungjui1251@gmail.com>
2019-09-20 16:58:30 +0800

    add comments to inserter.cpp

--- C++98/inserter.cpp
+++ C++98/inserter.cpp
@@ -10,7 +10,7 @@ void print(const int& n)
 
 void print_foo(std::list<int>& foo)
 {
-    std::cout << "foo:";
+    std::cout << "foo: ";
     std::for_each(foo.begin(), foo.end(), print);
     std::cout << std::endl;
 }
@@ -18,28 +18,35 @@ void print_foo(std::list<int>& foo)
 int main()
 {
     std::list<int> foo, bar;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 3; i++) {
         foo.push_back(i);
         bar.push_back(i*10);
     }
 
+    /* foo = { 0, 1, 2 } */
     std::cout << "foo:";
     std::for_each(foo.begin(), foo.end(), print);
     std::cout << std::endl;
 
+    /* bar = { 0, 10, 20 } */
     std::cout << "bar:";
     std::for_each(bar.begin(), bar.end(), print);
     std::cout << std::endl;
 
     std::list<int>::iterator it = foo.begin();
-    advance(it, 3);
+    advance(it, 1);
 
+    /* foo = { 0, { 0, 10, 20 }, 1, 2 } */
     std::copy(bar.begin(), bar.end(), std::inserter(foo,it));
     print_foo(foo);
 
+    /* foo = { 0, 0, 10, 20, 1, 2, { 0, 10, 20 } } */
     std::copy(bar.begin(), bar.end(), std::back_inserter(foo));
     print_foo(foo);
 
+    /* foo = { { 0 }, 0, 0, 10, 20, 1, 2, 0, 10, 20 } 
+       foo = { { 10, 0 }, 0, 0, 10, 20, 1, 2, 0, 10, 20 }
+       foo = { { 20, 10, 0 }, 0, 0, 10, 20, 1, 2, 0, 10, 20 } */
     std::copy(bar.begin(), bar.end(), front_inserter(foo));
     print_foo(foo);
 

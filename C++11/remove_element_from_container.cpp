3626452ab6b88256456b0f33d47424a1b2291d57
MeowDada <chang.hungjui1251@gmail.com>
2019-09-24 15:52:35 +0800

    Add remove element from container

---
+++ C++11/remove_element_from_container.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> v = { 1, 2, 3, 4, 2, 5, 2, 6 };
+
+    auto print_int = [](const int& v) { std::cout << v << " "; };
+
+    std::cout << "vector<int> v = { 1, 2, 3, 4, 2 ,5, 2, 6 }\n";
+    
+    std::cout << "v.erase(std::remove(std::begin(v), std::end(v), 2), std::end(v))\n";
+    v.erase(std::remove(std::begin(v), std::end(v), 2), std::end(v));
+    std::for_each(std::begin(v), std::end(v), print_int);
+
+    std::cout << "v.erase(std::remove_if(std::begin(v), std::end(v), [](int i){ return i % 2 == 0; }), std::end(v))\n";
+    v.erase(std::remove_if(std::begin(v), std::end(v),
+        [](int i) { return i % 2 == 0; }), std::end(v));
+    std::for_each(std::begin(v), std::end(v), print_int);
+
+
+    return 0;
+}

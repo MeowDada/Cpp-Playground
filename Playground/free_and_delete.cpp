26b5cb3d9e6eaf19b86bfc021111bdbf9593bd3c
MeowDada <chang.hungjui1251@gmail.com>
2019-09-26 09:53:54 +0800

    Add more info

--- Playground/free_and_delete.cpp
+++ Playground/free_and_delete.cpp
@@ -36,11 +36,13 @@ int main()
 
     std::cout << "bar1 = new foo();\n";
     std::cout << "Can we do free(bar1)?\n";
+    std::cout << "Yes, but will not invoke deconstructor, which might lead to memory leaking\n";
     free(bar1);
 
     std::cout << "bar2 = (foo *)malloc(sizeof(foo));\n";
     bar2 = (foo *)malloc(sizeof(foo));
     std::cout << "Can we do delete bar2 ?\n";
+    std::cout << "Yes, and it will also invoke deconstructor\n";
     delete bar2;
 
 
